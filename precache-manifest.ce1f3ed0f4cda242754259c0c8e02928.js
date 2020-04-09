b0e4494fa8e93ace6dd398967320eb99a5ea9cc6
Aldo Huayhua <aldohuayhua@gmail.com>
2020-04-08 22:47:05 -0400

    Updates

---
+++ precache-manifest.ce1f3ed0f4cda242754259c0c8e02928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bae79aefe8ae91c49293d92cda0c768",
+    "url": "/clicky-game-with-react/index.html"
+  },
+  {
+    "revision": "4c47d268003346142580",
+    "url": "/clicky-game-with-react/static/css/main.60629aa8.chunk.css"
+  },
+  {
+    "revision": "ba0a8bf3f19441908fba",
+    "url": "/clicky-game-with-react/static/js/2.0c8dca71.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicky-game-with-react/static/js/2.0c8dca71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c47d268003346142580",
+    "url": "/clicky-game-with-react/static/js/main.b926fcd8.chunk.js"
+  },
+  {
+    "revision": "e72ede1158b21fd4d3e5",
+    "url": "/clicky-game-with-react/static/js/runtime-main.35be3019.js"
+  }
+]);
